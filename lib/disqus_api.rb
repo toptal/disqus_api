0e371177c62d35dfdf115a929a1e95597103579f
Sergei Zinin <zinin@xakep.ru>
2013-12-09 16:33:21 +0800

    Fixes Hash core ext

--- lib/disqus_api.rb
+++ lib/disqus_api.rb
@@ -1,4 +1,4 @@
-require 'active_support/hash_with_indifferent_access'
+require 'active_support/core_ext/hash/indifferent_access'
 require 'active_support/core_ext/object/try'
 require 'yaml'
 require 'json'
