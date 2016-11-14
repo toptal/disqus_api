409f708c5392ded9535b6ec94ab494b35820bb70
Sasha Cooper <jinksy@gmail.com>
2016-11-14 11:30:35 +0000

    Allow disqus_api.yml to embed values with ERB

--- lib/disqus_api/railtie.rb
+++ lib/disqus_api/railtie.rb
@@ -4,7 +4,7 @@ module DisqusApi
       config_path = File.join(Rails.root, 'config', "disqus_api.yml")
 
       if File.exist?(config_path)
-        DisqusApi.config = YAML.load_file(File.join(Rails.root, 'config', "disqus_api.yml"))[Rails.env]
+        DisqusApi.config = YAML.load(ERB.new(File.read(File.join(Rails.root, 'config', "disqus_api.yml"))).result)[Rails.env]
       else
         unless Rails.env.test?
           puts "WARNING: No config/disqus_api.yml provided for Disqus API. Make sure to set configuration manually."
