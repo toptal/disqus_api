90667d3b5ff4b2965f88871580695991a73d322d
Maxim Vysotskiy <v.maksim.v@gmail.com>
2018-08-06 23:11:29 +0300

    Allow aliases in YAML config
    Support referencing repeated nodes with aliases, which is disallowed by
    `YAML.safe_load`. The typical use case for this is sharing
    configuration settings across environments.

--- lib/disqus_api/railtie.rb
+++ lib/disqus_api/railtie.rb
@@ -1,12 +1,10 @@
 module DisqusApi
   class Railtie < Rails::Railtie
     initializer 'disqus_api.initialize' do
-      config_path = File.join(Rails.root, 'config', "disqus_api.yml")
+      config_path = Rails.root.join('config', 'disqus_api.yml')
 
-      if File.exist?(config_path)
-        DisqusApi.config = YAML.safe_load(ERB.new(
-          File.read(Rails.root.join("config", "disqus_api.yml"))
-        ).result)[Rails.env]
+      if config_path.exist?
+        DisqusApi.config = YAML.load(ERB.new(config_path.read).result)[Rails.env]
       else
         unless Rails.env.test?
           puts "WARNING: No config/disqus_api.yml provided for Disqus API. Make sure to set configuration manually."
